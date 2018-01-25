5e2640d3497c1694beaa6ce07d662d5117f378d8
torkildl <torkild.lyngstad@gmail.com>
2018-01-25 11:59:51 +0100

    Added data for S2

---
+++ sos2900-seminar-2.R
@@ -0,0 +1,46 @@
+###
+### Seminar 2 regression and prediction using different data
+###
+source("http://bit.ly/SOS2900-startup-R")
+###
+### Load some necessary packages
+library(tidyverse)
+library(gapminder)
+library(here)
+library(MLmetrics)
+
+### Read in the data we want to use
+thedata <- read.csv("https://raw.githubusercontent.com/torkildl/sos2900-algorithms/master/materials/community_crime.csv")
+
+ 
+
+
+summary(thedata)
+
+
+# Plan for seminar 2
+
+# Load the data on crime and communities
+
+# Inspect the data
+
+# Choose outcome: murder rate
+
+# Model murder rate with specifically chosen one or two characteristics
+
+# Predict on testing data
+
+# Examine prediction
+
+# Model again with two vars. students choose variables (supervised learning)
+
+# Predict on testing data
+
+# Examine predictions
+
+# Model again using as many as you'd like
+
+# Predict on testing data
+
+# examine predictions
+
